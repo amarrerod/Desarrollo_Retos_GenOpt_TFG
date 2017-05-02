439747dde334f257f0b60e047ea46b61f5fe7959
Alejandro Marrero <alu0100825008@ull.edu.es>
2017-05-02 16:49:40 +0100

    Tercer commit: Solucionado un problema en el script que rellenaba los archivos desordenados

--- code/metrics.py
+++ code/metrics.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
 import os, sys, re, statistics
-from itertools import groupby
+from itertools import groupby, chain
 from collections import defaultdict, OrderedDict
 
-def computeMetrics(dir):
+def computeMetrics(source_dir, destiny_dir):
 	problems = defaultdict(list)
- 	for filename in os.listdir(dir):
+ 	for filename in os.listdir(source_dir):
 		if filename.endswith(".txt"):	
  			basename, extension = os.path.splitext(filename)
  			print "#Debug basename: {}".format(basename)
@@ -14,33 +14,40 @@ def computeMetrics(dir):
  			problems[problem].append(filename) # Agrupamos todos los nombres de ficheros del mismo problema
  	for problem_id, problem_files in problems.iteritems(): # problem_files es un array con todos los ficheros
  		problem_files.sort()
- 		metric_median = dir + "/median/median_" + str(problem_id) + ".metric"
- 		metric_boxplot = dir + "/boxplots/boxplots_" + str(problem_id) + ".metric"
+ 		metric_median = destiny_dir + "/median/median_" + str(problem_id) + ".metric"
+ 		metric_boxplot = destiny_dir + "/boxplots/boxplots_" + str(problem_id) + ".metric"
  		print "#Debug files to open {}, {}".format(metric_boxplot, metric_median)
  		boxplot_file = open(metric_boxplot, "a")
  		median_file = open(metric_median, "a")
  		index = 1
+ 		median_results = defaultdict(list)
+ 		# Recorremos todos los ficheros del problema 1-100
  		for filename in problem_files:
  			file = open(filename, "r")
-			median_results = defaultdict(list)
 			index = 3
+			# Leemos todas las lineas de cada fichero
 			while index < 49:
-				line = file.readlines()[index].decode()
+				line = file.readlines()[index]
 				file.seek(0, 0)
 				splitted = re.split(r'\t+', line.rstrip('\t'))
 				check_point = splitted[1]
-				value = splitted[-1]
-				print "#Debug index: {}, line: {}, check_point {}, value: {}".format(index, line, check_point, value)
+				value = float(splitted[-1])
+				print "#Debug index: {}, check_point {}, value: {}".format(index, check_point, value)
 				median_results[check_point].append(value)
-				if index == 48:
-					boxplot_file.write(value)
+				print "#Debug nuevo valor! checkpoint: {} value: {}".format(check_point, median_results[check_point])
+				if index == 48: ##Ultima linea del fichero --> Valor final
+					boxplot_file.write(str(value))
 					print "#Debug line_to_write in boxplot: {}  ".format(line)
 				index = index + 1
-			 #median_results = OrderedDict(sorted(median_results.items()))
-			for check_point, median in median_results.iteritems():
-				line = str(check_point) + "  {}  ".format(statistics.median(median_results[check_point]))
-				print "#Debug line_to_write: {}  ".format(line)
-				median_file.write(line)
+				print "\n"
+		# Rellenamos los ficheros de metricas para cada problema
+		for check_point in sorted(median_results.keys(), key = lambda x: int(x)):
+			print "#Debug check_point {}".format(check_point)
+			print "#Debug valores: {}".format(median_results[check_point]) 
+			line = " " + check_point + "  {}  \n".format(statistics.median(median_results[check_point]))
+			print "#Debug line_to_write: {}  ".format(line)
+			median_file.write(line)
+		median_results.clear() # Vaciamos el diccionario
 
 def createDirsAndFiles(dir):
 		boxplots = dir + "/boxplots"
@@ -58,12 +65,12 @@ def createDirsAndFiles(dir):
 			open(median_i, 'a').close()
 
 def main(argv):
-	if(len(argv) != 1):
-		print "Error al iniciar el script. \n Usage: python metrics.py dir"
+	if(len(argv) != 2):
+		print "Error al iniciar el script. \n Usage: python metrics.py source-dir destiny-dir"
 	else:
-		print "#Debug: Dir : {}".format(argv[0])
-		createDirsAndFiles(argv[0])
-		computeMetrics(argv[0])
+		print "#Debug: source: {}, destiny: {}".format(argv[0], argv[1])
+		createDirsAndFiles(argv[1])
+		computeMetrics(argv[0], argv[1])
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
